582130336ea6c067a17ee3fd8a3d57ab45b825d8
Darwin Gualotuña <daosgc@gmail.com>
2023-03-01 21:12:36 -0500

    Add computed properties, watchers, methods on heroes component

--- src/components/Heros.vue
+++ src/components/Heros.vue
@@ -6,7 +6,7 @@
     <div class="columns">
       <div class="column is-3">
         <header class="card-header">
-          <p class="card-header-title">heroes list</p>
+          <p class="card-header-title">Heroes List</p>
         </header>
         <ul class="list is-hoverable">
           <li v-for="hero in heroes" :key="hero.id">
@@ -23,7 +23,7 @@
     <div class="columns" v-if="selectedHero">
       <div class="column is-3">
         <header class="card-header">
-          <p class="card-header-title">{{ selectedHero.firstName }}</p>
+          <p class="card-header-title">{{ fullName }}</p>
         </header>
         <div class="card-content">
           <div class="content">
@@ -68,6 +68,19 @@
                 v-model="selectedHero.description"
               />
             </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="capeCounter">cape counter</label>
+              <input
+                class="input"
+                name="capeCounter"
+                type="number"
+                v-model="selectedHero.capeCounter"
+              />
+            </div>
+            <div class="field" v-show="showMore">
+              <label class="label" for="capeMessage">cape message</label>
+              <label class="input" name="capeMessage">{{ capeMessage }}</label>
+            </div>
           </div>
         </div>
       </div>
@@ -76,39 +89,99 @@
 </template>
 
 <script lang="ts">
+const heroes = [
+  {
+    id: 10,
+    firstName: "Ella",
+    lastName: "Papa",
+    description: "fashionista",
+    capeCounter: 2,
+  },
+  {
+    id: 20,
+    firstName: "Madelyn",
+    lastName: "Papa",
+    description: "the cat whisperer",
+    capeCounter: 0,
+  },
+  {
+    id: 30,
+    firstName: "Haley",
+    lastName: "Papa",
+    description: "pen wielder",
+    capeCounter: 1,
+  },
+  {
+    id: 40,
+    firstName: "Landon",
+    lastName: "Papa",
+    description: "arc trooper",
+    capeCounter: 2,
+  },
+];
+
 export default {
   name: "HeroesComponent",
   data() {
     return {
       selectedHero: undefined,
       showMore: false,
-      heroes: [
-        {
-          id: 10,
-          firstName: "Ella",
-          lastName: "Papa",
-          description: "fashionista",
-        },
-        {
-          id: 20,
-          firstName: "Madelyn",
-          lastName: "Papa",
-          description: "the cat whisperer",
-        },
-        {
-          id: 30,
-          firstName: "Haley",
-          lastName: "Papa",
-          description: "pen wielder",
-        },
-        {
-          id: 40,
-          firstName: "Landon",
-          lastName: "Papa",
-          description: "arc trooper",
-        },
-      ],
+      heroes: [],
+      capeMessage: "",
+      currentDate: new Date().valueOf(),
     };
+  },
+  created() {
+    this.loadHeroes();
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedHero?.firstName} ${this.selectedHero?.lastName}`;
+    },
+  },
+  methods: {
+    async getHeroes() {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(heroes), 1500);
+      });
+    },
+    async loadHeroes() {
+      this.heroes = await this.getHeroes();
+    },
+    handleTheCapes(newValue) {
+      const value = parseInt(newValue, 10);
+      switch (value) {
+        case 0:
+          this.capeMessage = "Where is my cape?";
+          break;
+        case 1:
+          this.capeMessage = "One is all I need";
+          break;
+        case 2:
+          this.capeMessage = "Alway have a spare";
+          break;
+        default:
+          this.capeMessage = "You can never have enough capes";
+          break;
+      }
+    },
+  },
+  watch: {
+    'selectedHero.capeCounter': {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log(`Watcher evalauted. old=${oldValue}, new=${newValue}`);
+        this.handleTheCapes(newValue);
+      },
+    },
+  },
+  filters: {
+    ShortDate: (value) => {
+      const day = new Date(value).getDay();
+      const month = new Date(value).getMonth();
+      const year = new Date(value).getFullYear();
+      return `${day}/${month}/${year}`;
+    },
   },
 };
 </script>
